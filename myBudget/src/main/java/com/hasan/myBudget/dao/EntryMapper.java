d5492cc5ee1477a3daa173c2de4403141d219f24
Hasan <careerforma@gmail.com>
2021-07-18 14:31:44 +0300

    refactor EntryMapper add all fields

--- myBudget/src/main/java/com/hasan/myBudget/dao/EntryMapper.java
+++ myBudget/src/main/java/com/hasan/myBudget/dao/EntryMapper.java
@@ -9,9 +9,12 @@ import java.sql.SQLException;
 public class EntryMapper implements RowMapper<Entry> {
     @Override
     public Entry mapRow(ResultSet resultSet, int i) throws SQLException {
+        var id = resultSet.getInt("id");
         var amount = resultSet.getInt("amount");
         var type = resultSet.getString("type");
+        var date = resultSet.getString("date");
+        var description = resultSet.getString("description");
 
-        return new Entry(amount, type);
+        return new Entry(id, amount, type, description, date);
     }
 }

0d0c4925490f4163a969af58bc5f3deefa1d36b6
Hasan <careerforma@gmail.com>
2021-07-18 14:22:42 +0300

    inject dao

--- myBudget/src/main/java/com/hasan/myBudget/dao/EntryDataAccess.java
+++ myBudget/src/main/java/com/hasan/myBudget/dao/EntryDataAccess.java
@@ -3,9 +3,11 @@ package com.hasan.myBudget.dao;
 import com.hasan.myBudget.model.Entry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository("EntryDao")
 public class EntryDataAccess implements EntryDao{
 
     @Autowired
--- myBudget/src/main/java/com/hasan/myBudget/service/EntryService.java
+++ myBudget/src/main/java/com/hasan/myBudget/service/EntryService.java
@@ -1,8 +1,16 @@
 package com.hasan.myBudget.service;
 
+import com.hasan.myBudget.dao.EntryDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EntryService {
+    @Autowired
+    EntryDao entryDao;
 
+    public EntryService(@Qualifier("EntryDao") EntryDao entryDao) {
+        this.entryDao = entryDao;
+    }
 }
